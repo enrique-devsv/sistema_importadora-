55ee32b825ea99fc34143e5a33e4189d1aa25cdb
a.carranza <abraham0699@gmail.com>
2026-05-01 14:28:39 -0600

    Funcion totalmente finalizada

--- clientes_module.py
+++ clientes_module.py
@@ -1,5 +1,12 @@
 from db_connection import get_connection
 
+CAMPOS_PERMITIDOS = {
+    "Nombres": "Nombres",
+    "Apellidos": "Apellidos",
+    "Email": "Email",
+    "Telefono": "Telefono"
+}
+
 def crear_cliente(nombre, apellido, email, telefono):
     conn = get_connection()
     cursor = conn.cursor()
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from clientes_module import crear_cliente
+from clientes_module import (crear_cliente, obtener_cliente, obtener_cliente_por_id, actualizar_cliente, eliminar_cliente)
 
-crear_cliente("Carlos", "Lopez", "carlos1004@mail.com", "77777777")
+
+actualizar_cliente(7, "Juan", "Pérez", "juanperez@gmail.com", "9876543210")
+eliminar_cliente(7)
+actualizar_cliente(10, "Juan", "Pérez", "juanperez@gmail.com", "9876543210")
+
