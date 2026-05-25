a7c3e3e126e7c0abcb6d8e088274699ead990e28
a.carranza <abraham0699@gmail.com>
2026-05-25 10:52:21 -0600

    reportes finalizado

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-from proveedores_module import *
-actualizar_proveedor(7, "Tech Solutions", "El Salvador", "Carlos Carranza", "555-0000")
-obtener_proveedores()
+from reportes_modules import menu_reportes
+
+def main():
+
+    while True:
+
+        print("\n===== SISTEMA IMPORTADORA =====")
+        print("1. Reportes")
+        print("2. Salir")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            menu_reportes()
+
+        elif opcion == "2":
+            print("Saliendo...")
+            break
+
+        else:
+            print("Opción inválida")
+
+
+if __name__ == "__main__":
+    main()
